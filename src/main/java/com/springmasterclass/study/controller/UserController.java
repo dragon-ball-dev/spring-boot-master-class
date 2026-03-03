8cbfda25ac41395501ae19168cf616c89e90db23
Ace Nghiem <dinhnam.nghiem.2611@gmail.com>
2026-03-03 23:30:51 +0700

    feat: Lesson 7 ✨

--- src/main/java/com/springmasterclass/study/controller/UserController.java
+++ src/main/java/com/springmasterclass/study/controller/UserController.java
@@ -1,4 +1,42 @@
 package com.springmasterclass.study.controller;
 
+import com.springmasterclass.study.dto.request.UserRq;
+import com.springmasterclass.study.dto.response.UserRp;
+import com.springmasterclass.study.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api/v1/users")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<UserRp>> showListUsers() {
+        return new ResponseEntity<>(userService.index(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<String> creaetNewUser(@RequestBody UserRq userRq) {
+        userService.create(userRq);
+        return new ResponseEntity<>("Create a new user successfully", HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateUser(@PathVariable("id") String id,@RequestBody UserRq userRq){
+        userService.update(id, userRq);
+        return new ResponseEntity<>("Update a user successfully", HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable("id") String id) {
+        userService.delete(id);
+        return new ResponseEntity<>("Delete a user successfully", HttpStatus.OK);
+    }
 }
--- src/main/java/com/springmasterclass/study/entity/User.java
+++ src/main/java/com/springmasterclass/study/entity/User.java
@@ -1,14 +1,26 @@
 package com.springmasterclass.study.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Table(name = "tl_user")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder // Giúp cho các bạn có thể insert vào User một các dễ dàng hơn thay vì các bạn phải contructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
     private String name;
+    @Column(name = "address", length = 30)
+    private String address;
+    @Column(name = "phone", length = 10)
+    private String phone;
+    @Column(unique = true)
+    private String email;
     private String username;
     private String password;
 }
